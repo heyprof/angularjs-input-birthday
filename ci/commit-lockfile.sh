7f9b50048afafce5eca77f1939ded8f40e1c64fd
Poney <arthur.agombart@gmail.com>
2018-04-10 15:34:33 +0200

    chore(*): Clean update lockfile script

--- ci/commit-lockfile.sh
+++ ci/commit-lockfile.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
 
-echo `git status`
-echo `git diff`
-
-echo "Should package-lock.json be regenerated?"
-# if [[ $TRAVIS_PULL_REQUEST_BRANCH != *"greenkeeper"* ]]; then
-# 	# Not a GreenKeeper Pull Request, aborting
-# 	exit 0
-# fi
-
-echo "Cloning repo"
-git clone "https://"$GITHUB_TOKEN"@github.com/"$TRAVIS_REPO_SLUG".git" repo
-cd repo
-
-echo "Switching to branch $TRAVIS_PULL_REQUEST_BRANCH"
-git checkout $TRAVIS_PULL_REQUEST_BRANCH
-
-echo "Updating lockfile"
-npm i
+if [[ $TRAVIS_PULL_REQUEST_BRANCH != *"greenkeeper"* ]]; then
+	# Not a GreenKeeper Pull Request, aborting
+	exit 0
+fi
 
 if ! git diff-index --quiet HEAD --; then
-  echo "Commit and push package-lock.json"
+  echo "Commit and push lockfile"
   git config --global user.email "$GITHUB_EMAIL"
   git config --global user.name "Travis CI"
   git config --global push.default simple
 
   git add package-lock.json
-  git commit -m "chore(*): update yarn.lock"
+  git commit -m "chore(*): update lockfile"
   git push
 fi
